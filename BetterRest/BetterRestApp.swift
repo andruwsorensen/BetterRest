e84d4bce4e10bef0e302cb02211a97cd2e4736fa
andruw-sorensen <andruw.sorensen@gmail.com>
2024-02-01 15:10:21 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Andruw Sorensen on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
